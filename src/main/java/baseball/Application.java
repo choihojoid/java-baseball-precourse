6087b6eeafab14c04a6727b59a5a58a959e0500a
Choi Hojo <choihojoid@gmail.com>
2024-08-25 02:46:30 +0900

    feat: 3자리 수 입력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,29 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Application {
 
     private static final int minNum = 100;
     private static final int maxNum = 999;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int randNum = Randoms.pickNumberInRange(minNum, maxNum);
+        int inputNum = receiveNumber();
+    }
+
+    private static int receiveNumber() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String inputStr = bufferedReader.readLine();
+        int inputNum = Integer.parseInt(inputStr);
+
+        return inputNum;
     }
 
 }
